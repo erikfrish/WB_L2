fec72c1274688650d8c059a0e9ca0bac36b76719
erikfrish <erikfrish@gmail.com>
2023-10-25 01:51:45 +0400

    pattern02

--- pattern/02_builder/main.go
+++ pattern/02_builder/main.go
@@ -1,15 +1,32 @@
 package main
 
-// import (
-// 	"02_builder/pattern"
-// 	"fmt"
-// )
+import (
+	"02_builder/pattern"
+	"fmt"
+)
 
 /*
 Builder
+Giorgi -- это простой грузинский пицца_мастер, он может сделать вам любую пиццу,
+которую вы попросите, например пепперони, маргариту и т.д.
+Но он не ограничивается пиццей, как и любой профессиональный грузин он умеет делать хачапури,
+главное правильно попросить, но даже если вы попросите у него то, чего он не умеет,
+он не оставит вас без внимания и угостит хотя бы сигареткой.
+
+Giorgi -- PizzaBuilder, имеет метод Build(what),
+достаточно дать ему название и он из него сотворит нужный нам объект нужного типа, следуя своей,
+не известной нам, логике, причем не станет возвращать объект, пока не приготовит его полностью
 
 */
 
 func main() {
+	Giorgi := pattern.NewPizzaBuilder("Giorgi")
+	pizza := Giorgi.Build("Pepperoni")
+	fmt.Printf("\ntype of pizza: %T\npizza: %v\n", pizza, pizza)
 
+	pizza = Giorgi.Build("Adjarian")
+	fmt.Printf("\ntype of pizza: %T\npizza: %v\n", pizza, pizza)
+
+	pizza = Giorgi.Build("anything_else?")
+	fmt.Printf("\ntype of pizza: %T\npizza: %v\n", pizza, pizza)
 }
--- pattern/02_builder/pattern/02_builder.go
+++ pattern/02_builder/pattern/02_builder.go
@@ -5,3 +5,45 @@ package pattern
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+
+type PizzaBuilder struct {
+	building string
+}
+
+func NewPizzaBuilder(name string) *PizzaBuilder {
+	return &PizzaBuilder{
+		building: name,
+	}
+}
+
+func (pb *PizzaBuilder) Build(building string) any {
+	switch building {
+	case "Pepperoni":
+		return &Pizza{name: building, size_in_cm: 24, ingredients: []string{"Sausages", "Cheese"}}
+	case "Margarita":
+		return &Pizza{name: building, size_in_cm: 20, ingredients: []string{"Cheese", "Tomato"}}
+	case "FourCheeses":
+		return &Pizza{name: building, size_in_cm: 35, ingredients: []string{"Cheese1", "Cheese2", "Cheese3", "Cheese4"}}
+	case "Carbonara":
+		return &Pizza{name: building, size_in_cm: 25, ingredients: []string{"Bacon", "Cheese"}}
+	case "Adjarian":
+		return &Khachapuri{type_: "Tasty Adjarian Khachapuri"}
+	case "Imeruli":
+		return &Khachapuri{type_: "Tasty Imeruli Khachapuri"}
+	case "Lobiani":
+		return &Khachapuri{type_: "Tasty Lobiani Khachapuri"}
+	case "Megruli":
+		return &Khachapuri{type_: "Tasty Megruli Khachapuri"}
+	default:
+		return "cigarette"
+	}
+}
+
+type Pizza struct {
+	name        string
+	size_in_cm  int
+	ingredients []string
+}
+type Khachapuri struct {
+	type_ string
+}
